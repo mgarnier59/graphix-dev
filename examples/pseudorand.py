f49ad5df83c8b9711fcc50f2faf9d6605308ccc7
mgarnier59 <50111289+mgarnier59@users.noreply.github.com>
2026-06-17 08:53:38 +0200

    Update pseudorand.py

--- examples/pseudorand.py
+++ examples/pseudorand.py
@@ -8,9 +8,11 @@ from __future__ import annotations
 
 import math
 from itertools import combinations, starmap
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Literal, assert_never
 
+from matplotlib.lines import Line2D
 import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
 import networkx as nx
 import numpy as np
 import numpy.typing as npt
@@ -59,7 +61,7 @@ def build_brickwork_pattern(meas: dict[int, Measurement]) -> Pattern:
     inputs = [0, 5]
     outputs = [4, 9]
 
-    return OpenGraph(inside_graph, meas, inputs, outputs).to_pattern()
+    return OpenGraph(inside_graph, inputs, outputs, meas).to_pattern()
 
 
 def remove_corrections(pattern: Pattern, rng: Generator, prob: float) -> Pattern:
@@ -87,6 +89,9 @@ def remove_corrections(pattern: Pattern, rng: Generator, prob: float) -> Pattern
         # don't modify preparations and entanglements
 
         if command.kind == CommandKind.M:
+            if command.measurement.downcast_bloch().to_plane() != Plane.XY:  # chzeck that all measurements re bloch
+                print("This example only works for XY plane.")
+
             s_domain, t_domain = command.s_domain, command.t_domain
 
             # print("start prob")
@@ -94,10 +99,10 @@ def remove_corrections(pattern: Pattern, rng: Generator, prob: float) -> Pattern
                 remove_corr_util(s_domain, prob, rng),
                 remove_corr_util(t_domain, prob, rng),
             )
+
             new_command: Command = M(
                 node=command.node,
-                plane=command.plane,
-                angle=command.angle,
+                measurement=command.measurement,
                 s_domain=new_s_domain,
                 t_domain=new_t_domain,
             )
@@ -162,7 +167,7 @@ def compute_pairwise_fidelities(ens: Sequence[Statevec]) -> npt.NDArray[np.float
     """
     # starmap is an iterator
     # fromiter avoid converting to list ninse np.array(iterator) doesn't behave as expected
-    return np.fromiter(starmap(fidelity, combinations(ens, 2)), dtype=np.float64)
+    return np.fromiter(starmap(fidelity, combinations(ens, 2)), dtype=np.float64)  # convert to map?
 
 
 def compute_state_frame_potential(
@@ -216,20 +221,21 @@ def compute_state_frame_potential_std(
 
 
 def build_gadget(n: int) -> Pattern:
-    measurements1 = {i: Measurement(0, Plane.XY) for i in [1, 3, 5, 7]}
-    measurements1.update({i: Measurement(1 / 4, Plane.XY) for i in [0, 2, 6]})
-    measurements1.update({8: Measurement(1 / 2, Plane.XY)})
+    measurements1 = {i: Measurement.XY(0) for i in [1, 3, 5, 7]}  # Measurement.X
+    measurements1.update({i: Measurement.XY(1 / 4) for i in [0, 2, 6]})
+    measurements1.update({8: Measurement.XY(1 / 2)})
 
-    measurements2 = {i: Measurement(0, Plane.XY) for i in [0, 2, 6, 8]}
-    measurements2.update({i: Measurement(1 / 4, Plane.XY) for i in [1, 5, 7]})
-    measurements2.update({3: Measurement(1 / 2, Plane.XY)})
+    measurements2 = {i: Measurement.XY(0) for i in [0, 2, 6, 8]}
+    measurements2.update({i: Measurement.XY(1 / 4) for i in [1, 5, 7]})
+    measurements2.update({3: Measurement.XY(1 / 2)})
 
-    p1 = build_brickwork_pattern(measurements1)
+    p1 = build_brickwork_pattern(measurements1)  # check type
     p2 = build_brickwork_pattern(measurements2)
 
     # building the "B" block
     # input nodes of p2 to input nodes of p1 :
     pc, mapping_c = p1.compose(p2, {0: 4, 5: 9})  # mapping_c is needed to access it
+    # print(mapping_c)
     patt_B, mapping_B = pc.compose(p1, {0: mapping_c[4], 5: mapping_c[9]})
 
     if n < 2:
@@ -281,7 +287,7 @@ def build_E(nqubits: int, nlayers: int) -> Pattern:
 
 
 def run(
-    nqubits: int, nlayers: int, shots: int, rng: Generator, prob: float
+    nqubits: int, nlayers: int, shots: int, rng: Generator, prob: float, t_vals: npt.NDArray[np.int32]
 ) -> tuple[list[float], list[float]]:  # Sequence[BackendState]
     # patt = build_gadget(n=nqubits)
     patt = build_E(nqubits=nqubits, nlayers=nlayers)
@@ -304,37 +310,221 @@ def run(
     # print(compute_state_frame_potential(out_data, 1))
 
     fids = compute_pairwise_fidelities(out_data)
-    print(f"{fids=}")
+    print(f"{fids=}")  # should just return fidelities. post proc after?
+    # or just the states?
 
-    return [compute_state_frame_potential(fids, t) for t in range(1, 4)], [
-        compute_state_frame_potential_std(fids, t) for t in range(1, 4)
-    ]  # out_data
+    return [compute_state_frame_potential(fids, t) for t in t_vals], [
+        compute_state_frame_potential_std(fids, t) for t in t_vals
+    ]  # out_data rework this. Std computed after data is generated?
 
 
-def plot_result(data: list[list[float]], x: npt.NDArray[np.float64], stds: list[list[float]]) -> None:
-    # data is list of 3-tuples
+def plot_result(
+    data: npt.NDArray[np.float64],
+    stds: npt.NDArray[np.float64],
+    p_vals: npt.NDArray[np.float64],
+    t_vals: npt.NDArray[np.int32],
+    dim: int,
+) -> None:  # TODO update unpacking to plat as many as necessary
+    """_summary_
 
-    # Unpack: Each variable now holds all values of one tuple position
-    curve1, curve2, curve3 = zip(*data)  # Adjust based on your tuple size
-    std1, std2, std3 = zip(*stds)
+    Parameters
+    ----------
+    data : npt.NDArray[np.float64]
+        _description_
+    x : npt.NDArray[np.float64]
+        _description_
+    stds : npt.NDArray[np.float64]
+        _description_
+    dim : int
+        dimension required for computing the analytical lower bounds
+    t_vals : npt.NDArray[np.int32]
+        _description_
+    var : Literal["t";, "p"], optional
+        x axis variable, by default "p" for removal probability, else "t" for polynomial degree
+    """
 
     # Theoretical reference lines for each curve
-    theoretical_values = [1 / math.comb(d + t - 1, t) for t in range(1, 4)]  # Replace with your theoretical values
+    theoretical_values = [compute_analytical_bound(dim, t) for t in t_vals]
 
-    # Plot each unpacked sequence
-    plt.errorbar(x, curve1, fmt="+r", capsize=2, elinewidth=0.9, yerr=std1, label=r"$t = 1$", ms=10)
-    plt.errorbar(x, curve2, fmt="bx", capsize=2, elinewidth=0.9, yerr=std2, label=r"$t = 2$", ms=10)
-    plt.errorbar(x, curve3, fmt="g2", capsize=2, elinewidth=0.9, yerr=std3, label=r"$t = 3$", ms=15)
-    colors = ["r", "b", "g"]
+    styles = ["+r", "bx", "g2"] + ["" for _ in range(len(data) - 3)]
+
+    for i in range(len(t_vals)):
+        plt.errorbar(
+            p_vals,
+            data[:, i],
+            fmt=styles[i],
+            capsize=2,
+            elinewidth=0.9,
+            yerr=stds[:, i],
+            label=rf"$t = {t_vals[i]}$",
+            ms=10,
+        )
+
+    colors = ["r", "b", "g"] + ["" for _ in range(len(data) - 3)]
     # Plot horizontal (theoretical) lines
     for idx, yval in enumerate(theoretical_values):
-        plt.axhline(y=yval, linestyle=":", label=rf"Lower bound $t={idx + 1}$", color=colors[idx])
-
+        plt.axhline(y=yval, linestyle=":", label=rf"Analytical bound $t={t_vals[idx]}$", color=colors[idx])
+    # plt.ylim(-0.01, 0.01)
     plt.legend()
     plt.xlabel("Correction removal probability")
-    plt.ylabel(r"$F_t$")
+    plt.ylabel(r"$\widetilde{F_t}$")
     plt.title(f"State frame potential for n = {nqb} qubits, {nlayers} layers and {nshots} shots")
     plt.show()
+
+
+# def plot_result_depth(
+#     data: npt.NDArray[np.float64],
+#     stds: npt.NDArray[np.float64],
+#     depth_vals: npt.NDArray[np.int32],
+#     t_vals: npt.NDArray[np.int32],
+#     dim: int,
+#     ylim: float | None = None
+# ) -> None:
+#     """_summary_
+
+#     Parameters
+#     ----------
+#     data : npt.NDArray[np.float64]
+#         _description_
+#     x : npt.NDArray[np.float64]
+#         _description_
+#     stds : npt.NDArray[np.float64]
+#         _description_
+#     dim : int
+#         dimension required for computing the analytical lower bounds
+#     t_vals : npt.NDArray[np.int32]
+#         _description_
+#     var : Literal["t";, "p"], optional
+#         x axis variable, by default "p" for removal probability, else "t" for polynomial degree
+#     """
+
+#     # Theoretical reference lines for each curve
+#     # theoretical_values = [compute_analytical_bound(dim, t) for t in t_vals]
+
+#     styles = [
+#         {"marker": "+", "color": "C0"},
+#         {"marker": "x", "color": "C1"},
+#         {"marker": "2", "color": "C2"},
+#         {"marker": "v", "color": "C3"},
+#     ]
+#     if len(styles) < len(t_vals): # rest will be ignored
+#         raise ValueError("Need as many styles are t values for plotting!")
+#     # styles += [{} for _ in range(len(t_vals) - len(styles))]
+#     # ["+", "bx", "g2"] + ["" for _ in range(len(data) - 3)]
+#     # styles = Line2D.markers
+#     # print(styles)
+#     for i in range(len(t_vals)):
+#         analyt_val = compute_analytical_bound(dim, t_vals[i])
+#         plt.plot(
+#             depth_vals,
+#             (data[:, i] - analyt_val) / analyt_val,
+#             **styles[i],
+#             # capsize=2,
+#             # elinewidth=0.9,
+#             # yerr=stds[:, i],
+#             label=rf"$t = {t_vals[i]}$",
+#             ms=10
+#         )
+#     plt.gca().xaxis.set_major_locator(MaxNLocator(integer=True))
+#     # colors = ["r", "b", "g"] + ["" for _ in range(len(data) - 3)]
+#     # Plot horizontal (theoretical) lines
+#     # for idx, yval in enumerate(theoretical_values):
+#     plt.axhline(y=0, linestyle=":", color="black")
+#     if ylim is not None:
+#         if not isinstance(ylim, float):
+#             raise TypeError(f"ylim must be a float not {type(ylim)}")
+
+#         plt.ylim(-0.01, ylim)
+#     plt.legend()
+#     plt.xlabel("Number of layers")
+#     # plt.ylabel(r"$\widetilde{F_t} - F_t$")
+#     plt.ylabel("State frame potential relative error")
+#     plt.title(f"n = {nqb} qubits and {nshots} samples")
+#     plt.tight_layout()
+#     if ylim is None:
+#         plt.savefig("pseudorand_final.pdf", format='pdf')
+#     else:
+#         plt.savefig(f"pseudorand_final_{ylim}.pdf", format='pdf')
+#     plt.show()
+
+def plot_result_depth(
+    data: npt.NDArray[np.float64],
+    stds: npt.NDArray[np.float64],
+    depth_vals: npt.NDArray[np.int32],
+    t_vals: npt.NDArray[np.int32],
+    dim: int,
+    ylim: float | None = None
+) -> None:
+
+
+    styles = [
+        {"marker": "+", "color": "C0"},
+        {"marker": "x", "color": "C1"},
+        {"marker": "2", "color": "C2"},
+        {"marker": "v", "color": "C3"},
+    ]
+
+    if len(styles) < len(t_vals):
+        raise ValueError("Need as many styles are t values for plotting!")
+
+    fig, ax = plt.subplots()
+
+    for i in range(len(t_vals)):
+        analyt_val = compute_analytical_bound(dim, t_vals[i])
+        y = (data[:, i] - analyt_val) / analyt_val
+        ax.plot(depth_vals, y, marker=styles[i]["marker"], color=styles[i]["color"], label=rf"$t = {t_vals[i]}$", ms=10)
+
+    ax.xaxis.set_major_locator(MaxNLocator(integer=True))
+    ax.axhline(y=0, linestyle=":", color="black")
+    ax.legend()
+    ax.set_xlabel("Number of layers")
+    ax.set_ylabel("State frame potential relative error")
+    ax.set_title(f"n = {nqb} qubits and {nshots} samples")
+
+    if ylim is not None and not isinstance(ylim, float):
+        raise TypeError(f"ylim must be a float not {type(ylim)}")
+
+    # Add inset showing the zoomed region
+    # inset: position [left, bottom, w, h]
+    # all in axes coordinates (0–1)
+    axins = ax.inset_axes((0.4, 0.55, 0.4, 0.4))  # adjust position/size as needed
+    for i in range(len(t_vals)):
+        analyt_val = compute_analytical_bound(dim, t_vals[i])
+        y = (data[:, i] - analyt_val) / analyt_val
+        axins.plot(depth_vals, y, marker=styles[i]["marker"], color=styles[i]["color"], ms=7,lw=1)
+
+    axins.set_ylim(-0.005, ylim)
+    axins.set_xlim(1.85, 5.15)
+    axins.xaxis.set_major_locator(MaxNLocator(integer=True))
+    axins.axhline(y=0, linestyle=":", color="black")
+    # ax.indicate_inset_zoom(axins, edgecolor="gray")
+
+    inset_indicator = ax.indicate_inset_zoom(axins, edgecolor="gray")
+
+    # connectors are (lower_left, upper_left, lower_right, upper_right)
+    ll, ul, lr, ur = inset_indicator.connectors
+
+
+    # ll.set_visible(False)
+    # lr.set_visible(False)
+    # ul.set_visible(True)
+    # ur.set_visible(True)
+    ll.set_visible(False)
+    ul.set_visible(True)
+    lr.set_visible(False)
+    ur.set_visible(True)
+
+    plt.tight_layout()
+    plt.savefig("pseudorand_final.pdf", format="pdf")
+    plt.show()
+
+
+
+
+def compute_analytical_bound(dim: int, t: int) -> float:
+    """compute the analytical value"""
+    # check power of 2.
+    return 1 / math.comb(dim + t - 1, t)
 
 
 # TODO
@@ -357,24 +547,49 @@ if __name__ == "__main__":
     # this is global
     # put everything in function! main
     nqb = 4
-    nlayers = 3
-    d = 2**nqb
-    nshots = 100
-    # t = 3
+    # nlayers = 2  # 3
+    dim = 2**nqb
+    nshots = int(1e3)
     rng = np.random.default_rng()
     res: list[list[float]] = []
     res_std: list[list[float]] = []
-    p_vals = np.linspace(0, 1, num=4, endpoint=True)
-    for p in tqdm(p_vals):
-        print(f"{p=}")
+    p_vals = np.array(
+        [1], dtype=np.float64
+    )  # np.array([1, 0], dtype=np.float64)  # [0,0.1,0.3,0.71] # np.linspace(0, 1, num=2, endpoint=True)
+    t_vals = np.array([1, 2, 3], dtype=np.int32)  # np.array(range(16,19), dtype=np.int32) 1, 5, 10
+    depth_vals = np.array([1, 2, 3, 4, 5], dtype=np.int32)  # np.array(range(16,19), dtype=np.int32)
+    # for p in tqdm(p_vals):
+    for depth in tqdm(depth_vals):
         data, stds = run(
-            nqubits=nqb, nlayers=nlayers, shots=nshots, rng=rng, prob=p
+            nqubits=nqb, nlayers=depth, shots=nshots, rng=rng, prob=p_vals[0], t_vals=t_vals
         )  # test prob + None and 1 should be the same
         res.append(data)
         res_std.append(stds)
     print(res)
-    print(f"analytical lower bounds {[1 / math.comb(d + t - 1, t) for t in range(1, 4)]}")
-    plot_result(data=res, x=p_vals, stds=res_std)
+    print(f"analytical lower bounds {[1 / math.comb(dim + t - 1, t) for t in t_vals]}")
+    # plot_result(
+    #     data=np.array(res, dtype=np.float64),
+    #     stds=np.array(res_std, dtype=np.float64),
+    #     p_vals=p_vals,
+    #     t_vals=t_vals,
+    #     dim=d,
+    # )
+    plot_result_depth(
+        data=np.array(res, dtype=np.float64),
+        stds=np.array(res_std, dtype=np.float64),
+        depth_vals=depth_vals,
+        t_vals=t_vals,
+        dim=dim,
+        ylim=4e-2
+    )
+    # plot_result_depth(
+    #     data=np.array(res, dtype=np.float64),
+    #     stds=np.array(res_std, dtype=np.float64),
+    #     depth_vals=depth_vals,
+    #     t_vals=t_vals,
+    #     dim=dim,
+    #     ylim=0.05
+    # )
     # logger.info('Finished')
     # print(len(data), data[0:2])
     # nx.draw_networkx(graph, with_labels=True) # add layers https://networkx.org/documentation/stable/reference/generated/networkx.drawing.layout.multipartite_layout.html
@@ -392,3 +607,9 @@ if __name__ == "__main__":
     #     print(out, fid)
 
     # print(remove_corrections(patt, rng, 0.5))
+
+
+# TODO : fix reproducibility (seed), small negative values for t= 1...plot abs?
+# add std dev too?
+# mention small negative value
+# fix x axis to integer
